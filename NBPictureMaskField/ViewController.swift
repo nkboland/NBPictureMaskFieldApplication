258c6ca707415a270563e415e685877c356557ba
Nick Boland <nick@bolandtech.com>
2016-03-27 10:52:28 -0400

    Initial Commit

---
+++ NBPictureMaskField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NBPictureMaskField
+//
+//  Created by Nick Boland on 3/27/16.
+//  Copyright © 2016 Nick Boland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
