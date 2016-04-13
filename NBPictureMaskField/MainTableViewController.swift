9b0aa75b962a2b7bf3120927e241f6ccd49650bf
Nick Boland <nick@bolandtech.com>
2016-04-12 22:42:10 -0400

    Add view with numerous examples.

---
+++ NBPictureMaskField/MainTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MainTableViewController.swift
+//  NBPictureMaskField
+//
+//  Created by Nick Boland on 4/12/16.
+//  Copyright © 2016 Nick Boland. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+//------------------------------------------------------------------------------
+
+  //--------------------
+  // MARK: - Overrides
+
+  override func viewDidLoad() {
+  //----------------------------------------------------------------------------
+    super.viewDidLoad()
+/*
+    let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(SimulatorConfigTVC.dismissKeyboard))
+    view.addGestureRecognizer(tap)
+
+    updateView(wasEdited: false)
+    updateView()
+*/
+  }
+
+  override func didReceiveMemoryWarning() {
+  //----------------------------------------------------------------------------
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
