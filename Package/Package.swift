b7b7710215f0cab5196edfbd82addb5b8dd75d65
Nick Boland <nick@bolandtech.com>
2016-09-30 00:50:42 -0400

    Reorganize some folders to help work better with Swift Package Manager.

---
+++ Package/Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  NBPictureMaskField
+//
+//  Created by Nick Boland on 9/30/16.
+//  Copyright © 2016 Nick Boland. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "NBPictureMaskField"
+)
